6ae6076b56648eb993c5a2cffb9a0f9aa93fa643
EucheriaK <ekureva@gmail.com>
2023-01-30 13:03:51 +0200

    more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-"""
-Defines a class Rectangle
+ a class Rectangle
 """
 
 
@@ -37,4 +35,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-	self.__height = value
+        self.__height = value
