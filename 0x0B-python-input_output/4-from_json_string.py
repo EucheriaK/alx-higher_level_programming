cdf6685b565ee1cf036bf19e35703667417ce100
EucheriaK <ekureva@gmail.com>
2023-02-07 13:03:52 +0200

    input output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This module defines a JSON-to-object function"""
-
+"""
+Contains the "from_json_string" function
+"""
 
 import json
 
 
 def from_json_string(my_str):
-    """Returns the Python object representation of a JSON string"""
+    """returns an object represented by a JSON string"""
     return json.loads(my_str)
-
