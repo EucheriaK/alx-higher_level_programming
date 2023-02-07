b348986b079b9e4cf69a3326b4e00b4db49b2a96
EucheriaK <ekureva@gmail.com>
2023-02-07 13:11:01 +0200

    input output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""my module"""
+""" Student to JSON """
 
 
 class Student:
-    """class of student"""
+    """ New class student """
 
     def __init__(self, first_name, last_name, age):
-        """initalizing parameters
-        Args:
-            first_name(str) = first given name of student
-            last_name(str) = last name of  student
-            age(int) = age of the student
-        """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """dict representation of student
-        Args:
-        attrs(list) = attribute too represent
-        """
-        if (type(attrs) == list and all(
-                type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
