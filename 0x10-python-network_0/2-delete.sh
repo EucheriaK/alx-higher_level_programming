3597c969ed08f8594cbcff17aaa4a22577e98151
EucheriaK <ekureva@gmail.com>
2023-04-27 11:49:07 +0200

    network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# send a DELETE request to the URL passed as the first argument and displays the body of the response
-
+# send  DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
 
