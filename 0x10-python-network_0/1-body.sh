a825585ddf80553fe945eb815ede322878858e17
EucheriaK <ekureva@gmail.com>
2023-04-27 11:45:28 +0200

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
-
+# displays the body of the response
+#!/bin/bash
+# displays the body of the response
 curl -sL "$1"
